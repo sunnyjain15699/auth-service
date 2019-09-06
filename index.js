29af01dc537153dcfb7e8e0a031d89f14a7071f1
Black Sky <sunnyjain15699@gmail.com>
2019-09-07 01:37:02 +0530

    Initial CommiT

--- index.js
+++ index.js
@@ -1,20 +1,53 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
+// Why do we need to set global vars using app.use()
 
 
+require('./models/User')
+
+const keys = require('./config/keys');
+
+// passport for authentication logic
 const passport = require('passport');
 require('./config/passport')(passport);
 
+const auth = require('./routes/auth');
 
-const auth = require('./routes/auth')
 
+// Database Logic
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI, {
+ useNewUrlParser: true
+}).then((data) => console.log('MongoDb connected database : ', data.connections[0].name))
+.catch(err => console.log(err))
+
+
+// Middleware Logic
+
+app.use(cookieParser());
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized : false
+}))
+
+// Passport MIddleware Logic
+app.use(passport.initialize());
+app.use(passport.session());
+app.use('/auth', auth);
+
+// Route Logic
 app.get('/', (req, res)=>{
     res.send('it works');
 })
 
-app.use('/auth', auth)
 
+
+// POrt Logic
 const port = process.env.PORT || 5000
 app.listen(port , ()=>{
     console.log(`Server started at ${port}`);
